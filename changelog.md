0838c8102443e741ffa34a2b88b211d395646325
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-04-11 22:45:09 -0500

    v0.2

---
+++ changelog.md
@@ -0,0 +1,7 @@
+## v0.2
+- Reworked code
+- Added orb value multipliers
+- Extended functionality for entity system
+
+## v0.1
+- Initial commit
--- src/main/Game.svelte
+++ src/main/Game.svelte
@@ -35,7 +35,7 @@
 	onMount(()=>{
 		ctx = canvas.getContext("2d");
 		draw.set_ctx(ctx);
-		timer.subscribe((v)=>{ main_loop(v); });
+		timer.subscribe((v)=>{ main_loop(v); main_loop(v); });
 		
 		canvas.width = main.clientWidth;
 		canvas.height = main.clientHeight;
@@ -89,7 +89,7 @@
 			const query = [];
 			for (let i = 0; i < this.list.length; i++) {
 				const ent = this.list[i];
-				if (ent.tag == tag) query.push(ent);
+				if (ent.tag.includes(tag)) query.push(ent);
 			}
 			return query;
 		},
@@ -112,7 +112,11 @@
 					continue;
 				}
 				if (ent.enabled === false) continue;
-				if (typeof this.funcs[ent.tag] == "function") this.funcs[ent.tag](ent);
+				if (ent.tag.length <= 0) return;
+				for (let j = 0; j < ent.tag.split(" ").length; j++) {
+					const tag = ent.tag.split(" ")[j];
+					if (typeof this.funcs[tag] == "function") this.funcs[tag](ent);
+				}
 			}
 			if (to_dest.length <= 0) return;
 			to_dest.reverse()
@@ -128,6 +132,15 @@
 				build = build.concat(this.get_all(tag));
 			}
 			this.list = build;
+		},
+		apply(index, obj) {
+			const ent = this.list.at(index);
+			for (const k in obj) {
+				if (!Object.hasOwnProperty.call(obj, k)) continue;
+				const v = obj[k];
+				ent[k] = v;
+			}
+			return ent;
 		}
 	}
 	entities.bind("collider", (col)=>{
@@ -160,6 +173,17 @@
 		} else if (col.hit) col.hit = false;
 		draw.rect(x, y, w, h, color, fill)
 	});
+	entities.bind("dropper", (drop)=>{
+		const { x, y } = drop.drop_at;
+		if (orbs.ticks <= 0) {
+			orbs.list.push({
+				x, y,
+				collect: false,
+				lvl: 0,
+			});
+			// console.log(orbs);
+		}
+	});
 	entities.bind("trigger", (trig)=>{
 		const { x, y, w, h, enter, leave } = trig;
 		const { color, enter_color, fill, width } = trig.style;
@@ -181,17 +205,18 @@
 			draw.rect(x,y,w,h, color, fill, width);
 		}
 	});
+	const orb_lvl_colors = [ "#fff", "#ff8282", "#ffb482", "#fcff82", "#a9ff82", "#82ffbb", "#82f3ff", "#82aaff", "#a782ff", "#ff82fc"]
 	entities.bind("orbs", (orbs)=>{
 		const { list } = orbs;
 
 		for (let i = 0; i < list.length; i++) {
 			const orb = list[list.length - i - 1];
-			draw.circle(orb.x, orb.y, 5, "aqua");
-			if (orb.x < 920) orb.x++
-			else if (orb.y < 510) orb.y++;
+			draw.circle(orb.x, orb.y, 5, orb_lvl_colors[orb.lvl]);
+			if (orb.x < 920) orb.lvl += multiply("x", orb.x, ++orb.x);
+			else if (orb.y < 510) orb.lvl += multiply("y", orb.y, ++orb.y);
 			else {
 				orb.collect = true;
-				$cash++;
+				$cash += orb.lvl;
 			}
 		}
 
@@ -204,15 +229,28 @@
 		}
 
 		if (orbs.ticks <= 0) {
-			list.push({
-				x: 920,
-				y: 55,
-				collect: false,
-			});
+			// list.push({
+			// 	x: 920,
+			// 	y: 55,
+			// 	collect: false,
+			// });
 			orbs.ticks = 60;
 			// console.log(list);
 		}
 		orbs.ticks--;
+	});
+	const multiply = (xy, pos1, pos2)=>{
+		const mults = entities.get_all("multiplier");
+		for (const mult of mults) {
+			if (mult.xy != xy || mult.enabled === false) continue;
+			if (pos1 <= mult[xy]+mult.h/2 && pos2 > mult[xy]+mult.h/2) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+	entities.bind("multiplier", (mult)=>{
+
 	});
 	//#endregion
 	//#region | Triggers
@@ -250,22 +288,32 @@
 			}
 		}
 	};
-	triggers.add(100, 100, 50, 50, { activate(t){ t.destroy = true; }, }, { color: "#ff000099" });
-	entities.list.at(-1).id = "test_trigger";
-	entities.list.at(-1).trigger_info = "Testing Testing 123";
+	triggers.add(100, 100, 50, 50, { activate(t){ t.destroy = true; }, }, { color: "#ff000099" })
+	void entities.apply(-1, { id: "test_trigger", trigger_info: "Testing Testing 123" });
 	
 	triggers.add(800, 40, 30, 30, {}, { color: "#ff000099", enter_color: "#ff6b0099" });
-	entities.list.at(-1).id = "drop_trigger_1";
-	entities.list.at(-1).trigger_info = "Dropper 1";
+	void entities.apply(-1, { id: "drop_trigger_1", trigger_info: "Dropper 1" });
 	
 	triggers.add(800, 140, 30, 30, {activate(t) { entities.gid("dropper_2").enabled = entities.gid("drop_trigger_2").enabled = t.destroy = true }}, { color: "#ff000099", enter_color: "#ff6b0099" });
-	entities.list.at(-1).id = "unlock_drop_trigger_2";
-	entities.list.at(-1).trigger_info = "Unlock Dropper 2";
+	void entities.apply(-1, { id: "unlock_drop_trigger_2", trigger_info: "Unlock Dropper 2" });
+
+	triggers.add(800, 240, 30, 30, {activate(t) { entities.gid("dropper_3").enabled = entities.gid("drop_trigger_3").enabled = t.destroy = true }}, { color: "#ff000099", enter_color: "#ff6b0099" });
+	void entities.apply(-1, { id: "unlock_drop_trigger_3", trigger_info: "Unlock Dropper 3" });
 	
 	triggers.add(800, 140, 30, 30, {}, { color: "#ff000099", enter_color: "#ff6b0099" });
-	entities.list.at(-1).id = "drop_trigger_2";
-	entities.list.at(-1).trigger_info = "Dropper 2";
-	entities.list.at(-1).enabled = false;
+	void entities.apply(-1, { id: "drop_trigger_2", trigger_info: "Dropper 2", enabled: false });
+
+	triggers.add(800, 240, 30, 30, {}, { color: "#ff000099", enter_color: "#ff6b0099" });
+	void entities.apply(-1, { id: "drop_trigger_3", trigger_info: "Dropper 3", enabled: false });
+
+	triggers.add(800, 340, 30, 30, {activate(t) { entities.gid("mult_1").enabled = t.destroy = entities.gid("unlock_mult_trigger_2").enabled = true }}, { color: "#ff000099", enter_color: "#ff6b0099" });
+	void entities.apply(-1, { id: "unlock_mult_trigger_1", trigger_info: "Unlock Multiplier 1" });
+	
+	triggers.add(800, 340, 30, 30, {activate(t) { entities.gid("mult_2").enabled = t.destroy = entities.gid("unlock_mult_trigger_3").enabled = true; }}, { color: "#ff000099", enter_color: "#ff6b0099" });
+	void entities.apply(-1, { id: "unlock_mult_trigger_2", trigger_info: "Unlock Multiplier 2", enabled: false });
+
+	triggers.add(800, 340, 30, 30, {activate(t) { entities.gid("mult_3").enabled = t.destroy = true; }}, { color: "#ff000099", enter_color: "#ff6b0099" });
+	void entities.apply(-1, { id: "unlock_mult_trigger_3", trigger_info: "Unlock Multiplier 3", enabled: false });
 	//#endregion
 	//#region | Colliders
 	const colliders = {
@@ -281,26 +329,33 @@
 		}
 	}
 	colliders.add(860, 510, 120, 70, ()=>{}, { color: "#222" } );
-	entities.list.at(-1).id = "collector_block";
+	void entities.apply(-1, { id: "collector_block" });
 	
 	colliders.add(895, 510, 50, 10, ()=>{}, { color: "#444" } );
-	entities.list.at(-1).id = "collector_block_door";
+	void entities.apply(-1, { id: "collector_block_door" });
 	
 	colliders.add(895, 20, 50, 490, ()=>{}, { color: "#555" } );
-	entities.list.at(-1).id = "conveyor_1";
+	void entities.apply(-1, { id: "conveyor_1" });
 
 	entities.add({
 		tag: "orbs",
 		list: [],
 		ticks: 0,
 	});
+	const orbs = entities.list.at(-1);
 	
 	colliders.add(835, 40, 100, 30, ()=>{}, { color: "#444" } );
-	entities.list.at(-1).id = "dropper_1";
+	void entities.apply(-1, { id: "dropper_1", tag: "collider dropper", drop_at: { x: 920, y: 55 } });
 	
 	colliders.add(835, 140, 100, 30, ()=>{}, { color: "#444" } );
-	entities.list.at(-1).id = "dropper_2";
-	entities.list.at(-1).enabled = false;
+	void entities.apply(-1, { id: "dropper_2", tag: "collider dropper", enabled: false, drop_at: { x: 920, y: 155 }  });
+
+	colliders.add(835, 240, 100, 30, ()=>{}, { color: "#444" } );
+	void entities.apply(-1, { id: "dropper_3", tag: "collider dropper", enabled: false, drop_at: { x: 920, y: 255 }  });
+	
+	colliders.add(890, 350, 60, 15, ()=>{}, { color: "#444" } ); void entities.apply(-1, { id: "mult_1", tag: "collider multiplier", xy: "y", enabled: false });
+	colliders.add(890, 400, 60, 15, ()=>{}, { color: "#444" } ); void entities.apply(-1, { id: "mult_2", tag: "collider multiplier", xy: "y", enabled: false });
+	colliders.add(890, 450, 60, 15, ()=>{}, { color: "#444" } ); void entities.apply(-1, { id: "mult_3", tag: "collider multiplier", xy: "y", enabled: false });
 	//#endregion
 	//#region | Controller / Key Events
 	const contr = {
@@ -339,7 +394,8 @@
 	window.onblur = ()=> {
 		contr.a = contr.d = contr.w = contr.s = false;
 	}
-	//#endregionw
+	//#endregion
+	
 </script>
 
 <main bind:this={main}>
